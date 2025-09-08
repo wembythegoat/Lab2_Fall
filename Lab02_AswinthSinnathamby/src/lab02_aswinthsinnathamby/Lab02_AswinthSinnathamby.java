dae33ca7a22a580f39927a54c1a24f2e4f1a6c43
2472557 <2472557@host.docker.internal>
2025-09-08 09:50:38 -0400

    Test commit

---
+++ Lab02_AswinthSinnathamby/src/lab02_aswinthsinnathamby/Lab02_AswinthSinnathamby.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab02_aswinthsinnathamby;
+
+/**
+ *
+ * @author 2472557
+ */
+public class Lab02_AswinthSinnathamby {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
