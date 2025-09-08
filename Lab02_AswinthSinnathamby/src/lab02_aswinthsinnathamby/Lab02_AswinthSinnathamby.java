576cf37274846d92f6011c7166e8706d35561772
2472557 <2472557@host.docker.internal>
2025-09-08 10:10:37 -0400

    Set up the stage, created a scene, and wrote the title

--- Lab02_AswinthSinnathamby/src/lab02_aswinthsinnathamby/Lab02_AswinthSinnathamby.java
+++ Lab02_AswinthSinnathamby/src/lab02_aswinthsinnathamby/Lab02_AswinthSinnathamby.java
@@ -4,19 +4,33 @@
  */
 package lab02_aswinthsinnathamby;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 2472557
  */
-public class Lab02_AswinthSinnathamby {
+public class Lab02_AswinthSinnathamby extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("boy");
-        System.out.println("yoo");
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage){
+        BorderPane root = new BorderPane();
+        
+        Scene scene = new Scene(root, 250, 300);
+        
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Java Games");
+        primaryStage.show();
     }
     
 }
